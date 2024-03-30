fccc4e1ac5d34db73dfc1a95ffec9781e5d6b19e
José Renteria <120423283+joserenter1a@users.noreply.github.com>
2024-03-29 23:37:08 -0700

    Create 198.py

---
+++ dynamic_programming/198.py
@@ -0,0 +1,16 @@
+"""
+LeetCode #198. House Robber
+
+Author: Jose Renteria
+"""
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        p = [0] * (n + 1)
+        p[1] = nums[0]
+        for i in range(1, n):
+            p[i + 1] = max(p[i - 1] + nums[i], p[i])
+        return p[n]
