37cb315737380b40d8c89a404250909101a03887
José Renteria <120423283+joserenter1a@users.noreply.github.com>
2024-03-29 23:11:21 -0700

    Update palindromic.py

--- palindromic.py
+++ palindromic.py
@@ -1,17 +1,8 @@
-from typing import List
-def groupPalindrome(strs: List[str]) -> List[List[str]]:
-    pals = []
-    def is_pal(el):
-        return el == el[::-1]
-    
-    for el in strs:
-        if is_pal(el):
-            pals.append(el)
-    return pals
+"""
+Leetcode #269. Missing Number
 
-inp = ["eat","tea","tan","ate","nat","bat", "ata"]
-groupPalindrome(inp)
-
+Author: Jose Renteria
+"""
 def missingNumber(nums: List[int]) -> int:
     if len(nums) not in nums:
         return len(nums)
@@ -24,11 +15,3 @@ print(missingNumber([3,0,1]))
 print(missingNumber([0, 1]))
 print(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
 
-def groupAnagrams(strs):
-    anagrams = []
-    if len(strs) == 1:
-        return [strs]
-    
-
-print(groupAnagrams([""]))
-print(len([""]))
