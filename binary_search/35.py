c5b8399a2db159d308e32dc08d4e39fdb81455be
José Renteria <120423283+joserenter1a@users.noreply.github.com>
2024-03-29 23:37:56 -0700

    Create 35.py

---
+++ binary_search/35.py
@@ -0,0 +1,16 @@
+"""
+LeetCode #35.py Search Insert Position
+
+Author: Jose Renteria
+"""
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) 
+        
+        while low < high:
+            mid = (high + low) >> 1
+            if nums[mid] >= target:
+                high = mid
+            else:
+                low = mid + 1
+        return low
