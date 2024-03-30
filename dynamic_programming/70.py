cf568c05481127a3d2243ce61bb98d49a3ba6e90
José Renteria <120423283+joserenter1a@users.noreply.github.com>
2024-03-29 23:36:39 -0700

    Create 70.py

---
+++ dynamic_programming/70.py
@@ -0,0 +1,14 @@
+"""
+LeetCode #70. Climbing Stairs
+
+Author: Jose Renteria
+"""
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev = 1
+        prev2 = 0
+        for i in range(1, n+1):
+            curi = prev + prev2
+            prev2 = prev
+            prev = curi
+        return prev
